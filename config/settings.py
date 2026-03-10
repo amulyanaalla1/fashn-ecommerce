a31a28494462418b22dd70ee8ea939644551e4f4
amulya naalla <amulyanaalla1@gmail.com>
2026-03-10 23:31:52 +0530

    prepare Project for Deployement in Render

--- config/settings.py
+++ config/settings.py
@@ -146,6 +146,9 @@ STATICFILES_DIRS = [
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 LOGIN_REDIRECT_URL = '/'
